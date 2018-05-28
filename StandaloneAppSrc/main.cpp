203cdc036bf1a8a2ec01fc1c8b800d36d5631557
DarkCaster <dark.caster@outlook.com>
2018-05-28 20:47:26 +0300

    StandaloneAppSrc: added ctrl+c handling for windows to standalone test app

--- StandaloneAppSrc/main.cpp
+++ StandaloneAppSrc/main.cpp
@@ -1,7 +1,44 @@
+#include <windows.h>
+#include <tchar.h>
+#include <iostream>
+#include <signal.h>
 #include <cstdio>
+#include <chrono>
+#include <thread>
+#include "standalone_config.h"
+#include "main_loop.h"
 
-int main() {
-   int n = 10;
-   printf("hello world %d\n",n);
-   return 0;
+BOOL WINAPI ConsoleHandler(DWORD dwCtrlType);
+
+static volatile boolean hangup=false;
+
+int main()
+{
+    if (SetConsoleCtrlHandler((PHANDLER_ROUTINE)ConsoleHandler,TRUE) == FALSE)
+    {
+        printf("Unable to install handler!\n");
+        return -1;
+    }
+    printf("starting main loop\n");
+    while(!hangup)
+    {
+        std::this_thread::sleep_for(std::chrono::milliseconds(10));
+    }
+}
+
+BOOL WINAPI ConsoleHandler(DWORD CEvent)
+{
+    switch(CEvent)
+    {
+    case CTRL_C_EVENT:
+    case CTRL_BREAK_EVENT:
+    case CTRL_CLOSE_EVENT:
+    case CTRL_LOGOFF_EVENT:
+    case CTRL_SHUTDOWN_EVENT:
+        hangup=true;
+        break;
+    default:
+        break;
+    }
+    return TRUE;
 }
