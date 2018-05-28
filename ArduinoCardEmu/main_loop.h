1ae7f937e0b01b3985a6358edb34b22ce64bd664
DarkCaster <dark.caster@outlook.com>
2018-05-28 21:10:12 +0300

    ArduinoCardEmu: main_loop.h updated, use C naming for setup and loop methods

--- ArduinoCardEmu/main_loop.h
+++ ArduinoCardEmu/main_loop.h
@@ -1,7 +1,15 @@
 #ifndef MAIN_LOOP_H
 #define MAIN_LOOP_H
 
-extern void setup();
-extern void loop();
+#ifdef __cplusplus
+extern "C"{
+#endif
+
+void setup();
+void loop();
+
+#ifdef __cplusplus
+} // extern "C"
+#endif
 
 #endif
