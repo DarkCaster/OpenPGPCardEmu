5981f95a80ab5c48206fac939b686e00a1529ba4
DarkCaster <dark.caster@outlook.com>
2018-05-29 02:14:40 +0300

    fixup! ArduinoCardEmu: add LOG macro to main_loop.cpp

--- ArduinoCardEmu/main_loop.cpp
+++ ArduinoCardEmu/main_loop.cpp
@@ -3,11 +3,12 @@
     #include "standalone_config.h"
     #include "serial.h"
     #include <cstdio>
-#define LOG(format,...) ({printf(format,__VA_ARGS__); fflush(stdout);})
+#define LOG(...) ({printf(__VA_ARGS__); fflush(stdout);})
+
 //#define LOG(format,...) ({})
 #else
     #include <Arduino.h>
-    #define LOG(format,...) ({})
+    #define LOG(...) ({})
 #endif
 
 void setup() {
@@ -17,5 +18,9 @@ void setup() {
 void loop() {
     int val=Serial.read();
     if(val>=0)
+    {
         LOG("COM PORT READ: %d\n", val);
+        if(!Serial.write(val))
+            LOG("COM PORT WRITE FAILED!\n");
+    }
 }
