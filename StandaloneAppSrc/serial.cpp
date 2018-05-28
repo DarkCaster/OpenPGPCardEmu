cdea62dc29e9f3f18436c09edd55ca3ff5b65649
DarkCaster <dark.caster@outlook.com>
2018-05-28 22:15:11 +0300

    StandaloneAppSrc: add SerialDummy class draft - emulate Serial for Arduino

--- StandaloneAppSrc/serial.cpp
+++ StandaloneAppSrc/serial.cpp
@@ -1,2 +1,33 @@
 #include "serial.h"
+#include <cstdio>
 
+SerialDummy Serial;
+
+#if BUILD_PLATFORM==WINDOWS
+
+void SerialDummy::begin(unsigned long baud)
+{
+    hComm = CreateFile(COMPORTNAME, GENERIC_READ | GENERIC_WRITE, 0, NULL, OPEN_EXISTING, 0, NULL);
+    if (hComm == INVALID_HANDLE_VALUE)
+    {
+        printf("Error in opening serial port\n");
+        return;
+    }
+    printf("Serial port opened\n");
+    fflush(stdout);
+    dcbSerialParams.DCBlength = sizeof(dcbSerialParams);
+    dcbSerialParams.BaudRate = baud;
+    dcbSerialParams.ByteSize = 8;
+    dcbSerialParams.StopBits = ONESTOPBIT;
+    dcbSerialParams.Parity   = NOPARITY;
+    SetCommState(hComm, &dcbSerialParams);
+}
+
+void SerialDummy::end()
+{
+    CloseHandle(hComm);
+}
+
+#else
+    #error platform is not supported for now!
+#endif
--- StandaloneAppSrc/serial.h
+++ StandaloneAppSrc/serial.h
@@ -1,4 +1,39 @@
 #ifndef SERIAL_H_INCLUDED
 #define SERIAL_H_INCLUDED
 
+#if BUILD_PLATFORM==WINDOWS
+    #define COMPORTNAME "\\\\.\\COM2"
+    #include<windows.h>
+#else
+    #error platform is not supported for now!
+#endif
+
+#include <cstddef>
+#include <cstdint>
+
+class SerialDummy
+{
+private:
+#if BUILD_PLATFORM==WINDOWS
+    HANDLE hComm;
+    DCB dcbSerialParams;
+#endif
+public:
+    void begin(unsigned long baud);
+    void end();
+    int available(void);
+    int peek(void);
+    int read(void);
+    int availableForWrite(void);
+    void flush(void);
+    size_t write(uint8_t);
+    size_t write(unsigned long n) { return write((uint8_t)n); }
+    size_t write(long n) { return write((uint8_t)n); }
+    size_t write(unsigned int n) { return write((uint8_t)n); }
+    size_t write(int n) { return write((uint8_t)n); }
+    operator bool() { return true; }
+};
+
+extern SerialDummy Serial;
+
 #endif
