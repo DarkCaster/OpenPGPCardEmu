d06efb951adbf85fbf81ab537a51d9733df09900
DarkCaster <dark.caster@outlook.com>
2018-06-01 04:10:22 +0300

    ArduinoCardEmu: add CRC8 service method

--- ArduinoCardEmu/main_loop.cpp
+++ ArduinoCardEmu/main_loop.cpp
@@ -1,14 +1,19 @@
 #include "main_loop.h"
-#ifdef STANDALONE_APP
-    #include "standalone_config.h"
-    #include "serial.h"
-    #include <cstdio>
-#define LOG(...) ({printf(__VA_ARGS__); fflush(stdout);})
 
+#ifdef STANDALONE_APP
+
+#include "standalone_config.h"
+#include "serial.h"
+#include <cstdio>
+#define LOG(...) ({printf(__VA_ARGS__); fflush(stdout);})
 //#define LOG(format,...) ({})
+
 #else
-    #include <Arduino.h>
-    #define LOG(...) ({})
+
+#include <Arduino.h>
+#include "crc8.h"
+#define LOG(...) ({})
+
 #endif
 
 void setup() {
