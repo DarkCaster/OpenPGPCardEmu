2086c44cafe9ffac48ee69a7e36297e5b3ff40d2
DarkCaster <dark.caster@outlook.com>
2018-06-07 19:04:10 +0300

    ArduinoCardEmu: add CommHelper class-draft

--- ArduinoCardEmu/comm_helper.h
+++ ArduinoCardEmu/comm_helper.h
@@ -50,7 +50,7 @@ uint8_t comm_verify(const uint8_t * const cmdBuff, const uint8_t cmdSize );
 
 uint8_t comm_message(uint8_t * const cmdBuff, const uint8_t cmdMask, const uint8_t * const payload, const uint8_t plLen);
 
-/*class CommHelper
+class CommHelper
 {
   private:
     const HardwareSerial serial;
@@ -62,23 +62,10 @@ uint8_t comm_message(uint8_t * const cmdBuff, const uint8_t cmdMask, const uint8
     uint8_t comm_header_decode(const uint8_t * const cmdBuff); // return 0 - transmission error, >0 - payload size + CRC size
     uint8_t comm_verify(const uint8_t * const cmdBuff, const uint8_t cmdSize ); // return 0 - verification error, 1 - ok
     uint8_t comm_message(uint8_t * const cmdBuff, const uint8_t cmdMask, const uint8_t * const payload, const uint8_t plLen); // return - message size
-
-    
-// payload size
-uint8_t comm_get_payload_size(uint8_t totalSize)
-{
-  return totalSize>0 ? totalSize-CMD_CRC_SIZE : totalSize;
+    // payload size
+    uint8_t comm_get_payload_size(uint8_t totalSize) { return totalSize>0 ? totalSize-CMD_CRC_SIZE : totalSize; }
+    uint8_t* comm_get_payload(uint8_t* const cmdBuffPtr) { return cmdBuffPtr + CMD_HDR_SIZE; }
+    uint8_t comm_get_req_mask(const uint8_t * const cmdBuffPtr){ return *cmdBuffPtr & REQ_ALL_MASK; }
 };
-
-uint8_t* comm_get_payload(const uint8_t * const cmdBuffPtr)
-{
-  return cmdBuffPtr + CMD_HDR_SIZE;
-};
-
-uint8_t comm_get_req_mask(const uint8_t * const cmdBuffPtr)
-{
-  return *cmdBuffPtr & REQ_ALL_MASK;
-};
-};*/
 
 #endif
