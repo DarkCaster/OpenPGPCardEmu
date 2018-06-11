f04d9d76bee5a182ada68225c04d562b949caa84
DarkCaster <dark.caster@outlook.com>
2018-06-11 15:55:32 +0300

    ArduinoCardEmu: simplify resync logic, remove use of global "status" field from resync method

--- ArduinoCardEmu/main_loop.cpp
+++ ArduinoCardEmu/main_loop.cpp
@@ -6,7 +6,7 @@
 #include "arduino-defines.h"
 #include "serial.h"
 #include <cstdio>
-#define LOG(...) ({printf(__VA_ARGS__); fflush(stdout);})
+#define LOG(...) ({printf(__VA_ARGS__); printf("\n"); fflush(stdout);})
 //#define LOG(format,...) ({})
 #define SYNC_OK() ({})
 #define SYNC_ERR() ({})
@@ -26,75 +26,63 @@
 
 #define LED_SYNC LED_BUILTIN
 
-static int8_t status = 0;
+static uint8_t status = 0;
 static CommHelper commHelper(&Serial);
 
 void send_resync()
 {
-  LOG("send_resync: resync pending after serial protocol error\n");
+  LOG("send_resync: sending resync-pending notification after protocol error");
   commHelper.SendAnswer(AnsType::Resync,NULL,0);
-  status=-1;
   return;
 }
 
 void resync()
 {
-  if(status>0)
-    return;
-  if(status==0)
-    status--;
-  SYNC_ERR();
-  //read request
-  auto request=commHelper.ReceiveRequest();
-  LOG("resync in progress");
-  //check header
-  if(request.reqType==ReqType::Invalid)
+  LOG("Starting resync");
+  uint8_t resyncState=0;
+  while(true)
   {
-    LOG("resync: invalid request received");
-    send_resync();
-    return;
-  }
-  if(status==-2)
-  {
-    //if request is ResyncComplete
-    if(request.reqType==ReqType::ResyncComplete)
+    SYNC_ERR();
+    //read request
+    auto request=commHelper.ReceiveRequest();
+    LOG("Resync in progress");
+    //check header
+    if(request.reqType==ReqType::Invalid)
     {
-      //send ANS_OK
-      if(!commHelper.SendAnswer(AnsType::Ok,NULL,0))
+      LOG("resync: invalid request received");
+      send_resync();
+      resyncState=0;
+      continue;
+    }
+    if(resyncState==1)
+    {
+      //if request is ResyncComplete
+      if(request.reqType==ReqType::ResyncComplete)
       {
-        LOG("resync: failed to send Ok answer to ResyncComplete request");
-        send_resync();
+        //send ANS_OK
+        if(!commHelper.SendAnswer(AnsType::Ok,NULL,0))
+        {
+          LOG("resync: failed to send Ok answer to ResyncComplete request");
+          send_resync();
+          resyncState=0;
+          continue;
+        }
+        //resync complete!
+        LOG("Resync complete!");
+        SYNC_OK();
         return;
       }
-      //resync complete!
-      status=1;
-      LOG("resync complete!");
-      SYNC_OK();
-      return;
+      resyncState=0;
     }
-    else
-      status=-1;
-  }
-  if(status==-1)
-  {
-    //if header is not RESYNC, send ANS_RESYNC with 0 bytes payload, return
-    if(request.reqType!=ReqType::Resync)
+    //final resync-sequence
+    if(request.reqType!=ReqType::Resync || !commHelper.SendAnswer(AnsType::Resync,request.payload,request.plLen))
     {
-      LOG("resync: incorrect request type");
+      LOG("resync: incorrect request type or final resync-sequence send-failure");
       send_resync();
-      return;
+      continue;
     }
-    //reply with current request's payload
-    if(!commHelper.SendAnswer(AnsType::Resync,request.payload,request.plLen))
-    {
-      LOG("resync: failed to send final resync-sequence");
-      send_resync();
-      return;
-    }
-    status--;
+    resyncState=1;
   }
-  else
-    send_resync();
 }
 
 void setup()
@@ -107,7 +95,11 @@ void setup()
 void loop()
 {
   //try to perform resync
-  resync();
+  if(!status)
+  {
+    resync();
+    status=1;
+  }
   //TODO: read request
   //TODO: perform action
   //TODO: send response
