d134984d1f961b6e55d1ccee53710968f8bc83a2
DarkCaster <dark.caster@outlook.com>
2018-06-14 04:07:04 +0300

    ArduinoCardEmu: update serial-protocol logic, data write to card routine updated

--- ArduinoCardEmu/main_loop.cpp
+++ ArduinoCardEmu/main_loop.cpp
@@ -28,6 +28,7 @@
 #define LED_SYNC LED_BUILTIN
 
 static uint8_t status = 0;
+static uint16_t writePending = 0;
 static CommHelper commHelper(&Serial);
 static SmartCard smartCard;
 
@@ -123,7 +124,7 @@ void loop()
 {
   //read request
   auto request=commHelper.ReceiveRequest();
-  uint8_t answerResult=1;
+  uint8_t result=1;
   //perform action
   switch (request.reqType)
   {
@@ -133,45 +134,67 @@ void loop()
       //SET_CARD_STATUS(0);
       //SET_CARD_STATUS(1);
       //return answer
-      answerResult=commHelper.SendAnswer(IS_CARD_PRESENT?AnsType::CardPresent:AnsType::CardAbsent,NULL,0);
+      result=commHelper.SendAnswer(IS_CARD_PRESENT?AnsType::CardPresent:AnsType::CardAbsent,NULL,0);
       break;
     case ReqType::CardCommit:
-      smartCard.inBuffer.WriteCommit();
-      //may take a LONG TIME while performing requested operation
-      smartCard.Commit();
-      //send Ok answer
-      answerResult=commHelper.SendAnswer(AnsType::Ok,NULL,0);
+      if(writePending>0)
+      {
+        LOG("Internal error! ReqType::CardCommit: writePending>0!");
+        result=0;
+      }
+      else
+      {
+        smartCard.inBuffer.WriteCommit();
+        //may take a LONG TIME while performing requested operation
+        smartCard.Commit();
+        //send Ok answer
+        result=commHelper.SendAnswer(AnsType::Ok,NULL,0);
+      }
       break;
     case ReqType::CardReadComplete:
       //trim data from buffer that was succsesfully read before
       smartCard.outBuffer.ReadTrim();
       //send Ok answer
-      answerResult=commHelper.SendAnswer(AnsType::Ok,NULL,0);
+      result=commHelper.SendAnswer(AnsType::Ok,NULL,0);
       break;
     case ReqType::CardRead:
-      //rewind outgoing buffer in order to recover from previous read-error (if any)
+      //rewind outgoing buffer in order to recover from previous read-result (if any)
       smartCard.outBuffer.ReadRewind();
       //TODO: get outgoing-buffer data-size and compare it with requested data-size
       //TODO: read data from buffer and send it
       //send EOD
-      answerResult=commHelper.SendAnswer(AnsType::CardEOD,NULL,0);
+      result=commHelper.SendAnswer(AnsType::CardEOD,NULL,0);
       break;
     case ReqType::CardSend:
-      //TODO: write incoming data to card's inBuffer
-      //send Ok answer
-      answerResult=commHelper.SendAnswer(AnsType::Ok,NULL,0);
+      if(writePending>0)
+      {
+        //TODO: write incoming data to card's inBuffer, decrement writePending
+        //send Ok answer
+        result=commHelper.SendAnswer(AnsType::Ok,NULL,0);
+      }
+      else
+      {
+        //first send-command, encodes total bytes to be written
+        //rewind incoming buffer to recover from possible error
+        smartCard.inBuffer.WriteRewind();
+        //TODO: decode bytes to be writen and set it to writePending var
+        writePending=1;
+        //send Ok answer
+        result=commHelper.SendAnswer(AnsType::Ok,NULL,0);
+      }
       break;
     default:
       LOG("Incorrect request type");
     case ReqType::Invalid:
     case ReqType::Resync:
     case ReqType::ResyncComplete:
-      answerResult=0;
+      result=0;
       break;
   }
-  if(!answerResult)
+  if(!result)
   {
-    smartCard.inBuffer.WriteRewind(); // to recover from possible write-error
+    //reset write pending bytes
+    writePending=0;
     resync();
     return;
   }
